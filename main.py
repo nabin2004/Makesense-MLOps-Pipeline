8234a71a04b2f09220ff1d938c4dc0485f985414
nabin2004 <olinabin2004@gmail.com>
2026-01-10 18:29:04 +0545

    ADD: Data Transformation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.makesense.logging import logger
 from src.makesense.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
+from src.makesense.pipeline.stage_2_data_transformation_pipeline import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -13,5 +14,15 @@ except Exception as e:
     logger.exception(f"Error occurred in stage {STAGE_NAME}: {e}")
     raise e
 
+STAGE_NAME="Data Transformation stage"
+
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+    data_ingestion_pipeline=DataTransformationTrainingPipeline()
+    data_ingestion_pipeline.initiate_data_transformation()
+    logger.info(f"Stage {STAGE_NAME} Completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 logger.info("Logging has been configured.")
